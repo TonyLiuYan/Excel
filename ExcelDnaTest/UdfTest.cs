d140c1271caab312268da60ae69d50096688e012
Tony <345006662@qq.com>
2016-07-24 21:07:08 +0800

    1

---
+++ ExcelDnaTest/UdfTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ExcelDna.Integration;
+
+namespace ExcelDnaTest
+{
+
+    public class UdfTest
+    {
+        [ExcelFunction(Name = "aa")]
+        public static object aa()
+        {
+            var xlCall = XlCall.Excel(XlCall.xlGetInst);
+            return xlCall;
+        }
+
+        [ExcelFunction(Name = "bb")]
+        public static object bb()
+        {
+            var xlCall = XlCall.Excel(XlCall.xlGetHwnd);
+            return xlCall;
+        }
+
+        [ExcelFunction(Name = "cc")]
+        public static object cc()
+        {
+            var xlCall = XlCall.Excel(XlCall.xlModeEdit);
+            return xlCall;
+        }
+    }
+}
